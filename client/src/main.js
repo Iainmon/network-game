2fef0af27ba4d7a1244803b63b31e872af910c80
Matthias <matthiaskrh123@gmail.com>
2020-03-18 15:37:12 -1000

    mmmmmegege

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,28 @@
-import * as Matter from 'matter-js'
+import { Engine, Render, World, Events, Bodies, Body, Vector } from 'matter-js'
 import * as THREE from 'three'
 import Communicator from './communicator'
+import Game from './Game.client.js'
 
 
 
-const keys = {}
 
-const endpoint = 'localhost'
+
+const gameInstance = new Game()
+const render = Render.create({
+    element: document.body,
+    engine: gameInstance.engine
+});
+
+
+let keys = {}
+
+const endpoint = '192.168.1.167'
 const port = 3000
 const communicator = new Communicator(endpoint, port)
 communicator.onRecieve( (message) => {
-    // this.game = JSON.parse(message)
+    // console.log(message.data)
+    const packet = JSON.parse(message.data)
+    gameInstance.loadFromPacket(packet)
 })
 
 document.body.addEventListener('keydown', (event) => {
@@ -24,7 +36,8 @@ document.body.addEventListener('keyup', (event) => {
 });
 
 
-const looper = setInterval( () => {
+//const looper = setInterval( () => {
+//}, 16)
 
-    
-}, 16)
+
+Render.run(render);
